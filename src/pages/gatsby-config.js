66238a46c3dce93b6e47efc2026703bd9e700e06
Kumar <kumar.senkan@nuance.com>
2021-08-11 22:34:41 +0900

    Finished Gatsby Tutorial Part 3

---
+++ src/pages/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
